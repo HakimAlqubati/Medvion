d9856182484ca27c8eb0c77a8acc8d93bda29143
HakimAlqubati <hakimahmed123321@gmail.com>
2026-03-26 22:41:04 +0300

    reorder sections in home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,11 @@
 <x-layouts.frontend :title="__('land.title')">
 
     <x-frontend.hero />
-
     <x-frontend.features />
-
+    <x-frontend.latest-courses />
     <x-frontend.faq />
 
-    <x-frontend.latest-courses />
+
+
 
 </x-layouts.frontend>
