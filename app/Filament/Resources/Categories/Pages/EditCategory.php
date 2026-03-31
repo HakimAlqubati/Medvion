ca7f3213dc72b958140b78134deeccaac1ba621a
HakimAlqubati <hakimahmed123321@gmail.com>
2026-03-31 23:11:43 +0300

    add trans filament  for category pages

--- app/Filament/Resources/Categories/Pages/EditCategory.php
+++ app/Filament/Resources/Categories/Pages/EditCategory.php
@@ -7,14 +7,19 @@ use Filament\Actions\DeleteAction;
 use Filament\Actions\ForceDeleteAction;
 use Filament\Actions\RestoreAction;
 use Filament\Resources\Pages\EditRecord;
+use LaraZeus\SpatieTranslatable\Actions\LocaleSwitcher;
+use LaraZeus\SpatieTranslatable\Resources\Pages\EditRecord\Concerns\Translatable;
 
 class EditCategory extends EditRecord
 {
+    use Translatable;
     protected static string $resource = CategoryResource::class;
 
     protected function getHeaderActions(): array
     {
         return [
+            LocaleSwitcher::make(),
+
             DeleteAction::make(),
             ForceDeleteAction::make(),
             RestoreAction::make(),
