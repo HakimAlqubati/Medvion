bbaa0ce5a89a41a29439356cf452549d948b066b
HakimAlqubati <hakimahmed123321@gmail.com>
2026-05-07 01:38:34 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,11 @@
 <x-layouts.frontend :title="__('land.title')">
     <x-frontend.hero :slides="$slides" />
-    <x-frontend.testimonials :alt-bg="true" />
-    <x-frontend.partners />
-    <x-frontend.latest-courses :alt-bg="false" />
-    <x-frontend.blog :alt-bg="false" />
-    <x-frontend.academic-experts />
-    <x-frontend.features :alt-bg="true" :features="$features" />
     <x-frontend.about :alt-bg="false" />
+    <x-frontend.features :alt-bg="true" :features="$features" />
+    <x-frontend.latest-courses :alt-bg="false" />
     <x-frontend.faq :alt-bg="true" />
+    <x-frontend.partners />
+    <x-frontend.academic-experts />
+    <x-frontend.testimonials :alt-bg="true" />
+    <x-frontend.blog :alt-bg="false" />
 </x-layouts.frontend>
